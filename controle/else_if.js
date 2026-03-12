1bd1f37ead5e9028e6c26c1bcc8313411b856ca8
ana_legal <anactschaalmeida2@outlook.com>
2026-03-12 12:16:11 -0300

    Estrutura de controle

---
+++ controle/else_if.js
@@ -0,0 +1,14 @@
+// Desafio
+
+let nota = 5.0;
+const media = 7.5;
+
+if(nota >= 7.5){
+    console.log("Aprovado");
+
+} else if (nota < 5.0 && nota > media){
+     console.log("Recuperação");
+
+} else {
+        console.log ("Reprovado");
+    }
---
+++ controle/template_string.js
@@ -0,0 +1,7 @@
+let nome = "Ana";
+let idade = 16;
+// Imprimir no terminal o nome e a idade
+// Junto com uma frase.
+
+console.log("Meu nome é " + nome + "e tenho" + idade + "anos de idade");
+console.log(`Meu nome é  ${nome} e tenho ${idade} anos de idade.`);
