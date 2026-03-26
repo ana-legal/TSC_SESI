8a1297b01bbb386f2eb6304cd70330c9adbc9f08
ana_legal <anactschaalmeida2@outlook.com>
2026-03-26 11:39:48 -0300

    Atividade

---
+++ atividades/exercicio1.js
@@ -0,0 +1,10 @@
+let produto = "Caneta";
+console.log(typeof produto);
+
+let preco = 2;
+console.log(typeof preco);
+
+let estaEmEstoque = true; 
+console.log(typeof estaEmEstoque);
+
+console.log(`O item é  ${produto} custa R$${preco}`);
